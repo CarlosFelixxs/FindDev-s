33b1e59db9689b8e64f1f6f6f1392d4010596e0c
CarlosFelixxs <carlos.fsilva@bandtec.com.br>
2022-10-09 17:45:17 -0300

    Ajustes na home page

--- FrontEnd/src/pages/Home/index.tsx
+++ FrontEnd/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../shared/components/Header";
 import styles from './home.module.css'
 import imagemBanner from "../../assets/images/imgBanner.png";
-
+import { TopicOne, TopicTwo, TopicTree, TopicFour } from '../../shared/components/Topics'
 
 export default function Home() {
   return (
@@ -15,6 +15,21 @@ export default function Home() {
         </div>
         <div className={styles.containerImg}>
           <img className={styles.imgBanner} src={imagemBanner} alt="imagem do banner" />
+        </div>
+      </section>
+      <section className={styles.sectionTopics}>
+        <div className={styles.contH1}>
+          <h1 className={styles.h1Topics}>Por que escolher a FindDev?</h1>
+        </div>
+        <div className={styles.cont_tp}>
+          <div className={styles.sectionTp}>
+            <TopicOne />
+            <TopicTree />
+          </div>
+          <div className={styles.sectionTp1}>
+            <TopicTwo />
+            <TopicFour />
+          </div>
         </div>
       </section>
     </>
