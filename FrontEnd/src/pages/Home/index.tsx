2f05ff61953d3b0e7137a70400985c4d562542e8
CarlosFelixxs <carlos.fsilva@bandtec.com.br>
2022-10-15 17:11:50 -0300

    teste-banner

--- FrontEnd/src/pages/Home/index.tsx
+++ FrontEnd/src/pages/Home/index.tsx
@@ -1,23 +1,14 @@
 import { Header } from "../../shared/components/Header";
 import styles from './home.module.css'
-import imagemBanner from "../../assets/images/imgBanner.png";
 import { TopicOne, TopicTwo, TopicTree, TopicFour } from '../../shared/components/Topics'
 import { Cards } from "../../shared/components/Cards";
+import { Banner } from "../../shared/components/Banner";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className={styles.container}>
-        <div className={styles.container_banner}>
-          <h1 className={styles.h1}>conectando quem sabe com quem precisa</h1>
-          <p className={styles.p}>a plataforma ideal para encontrar seus desenvolvedores freelancers</p>
-          <button className={styles.btnComoFunciona}>como funciona</button>
-        </div>
-        <div className={styles.containerImg}>
-          <img className={styles.imgBanner} src={imagemBanner} alt="imagem do banner" />
-        </div>
-      </section>
+      <Banner />
       <section className={styles.sectionTopics}>
         <div className={styles.contH1}>
           <h1 className={styles.h1Topics}>Por que escolher a FindDev?</h1>
@@ -34,7 +25,7 @@ export default function Home() {
         </div>
       </section>
       <section className={styles.sectionCards}>
-      <div className={styles.contH1}>
+        <div className={styles.contH1}>
           <h1 className={styles.h1Topics}>E como funcionamos?</h1>
         </div>
         <div className={styles.contCards}>
