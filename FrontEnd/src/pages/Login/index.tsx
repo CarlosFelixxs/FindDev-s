af81ec0a4302b6f990770b71994b8d1b18ad8a4a
CarlosFelixxs <carlos.fsilva@bandtec.com.br>
2022-10-17 19:55:55 -0300

    Tela de login

--- FrontEnd/src/pages/Login/index.tsx
+++ FrontEnd/src/pages/Login/index.tsx
@@ -1,56 +1,48 @@
-import { useNavigate  } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.css';
-
 import { Header } from '../../shared/components/Header';
-
 import signupImage from "../../assets/images/signup-img.png";
 
 export default function Cadastro() {
 
   const navigate = useNavigate();
 
-  const routeChange = (path : string) =>{ 
+  const routeChange = (path: string) => {
     navigate(path);
   }
 
   return (
     <>
-      <Header isLoginScreen={false} isHomePage={false}/>
+      <Header isLoginScreen={true} isHomePage={false} />
       <div className={styles.container}>
         <div className={styles.left}>
           <div className={styles.text}>
             <div className={styles.title}>
-              OLÁ, BEM VINDO! <br />  
-              JÁ POSSUI UMA CONTA?
+              OLÁ, BEM VINDO DE VOLTA! <br />
+              AINDA NÃO POSSUI UMA CONTA?
             </div>
-            <div className={styles.subtitle} onClick={() => routeChange("/login")}>
-              REALIZE SEU LOGIN
+            <div className={styles.subtitle} onClick={() => routeChange("/cadastro")}>
+              REGISTRE-SE JÁ
             </div>
           </div>
-          <div className={styles.image}>
-            <img src={signupImage} alt="imagem do banner" />
+          <div className={styles.contImage}>
+            <img className={styles.image} src={signupImage} alt="imagem do banner" />
           </div>
         </div>
         <div className={styles.right}>
           <div className={styles.cardSignup}>
             <div className={styles.cardContent}>
-              <h1>CADASTRO</h1>
+              <h1>LOGIN</h1>
               <form className={styles.formSignup}>
                 <div className={styles.leabelInput}>
-                  <label>EMAIL</label>
+                  <label className={styles.titleInput}>EMAIL</label>
                   <div className={styles.separador}></div>
-                  <input type="text" className={styles.input} placeholder="exemplo@email.com"/>
+                  <input type="text" className={styles.input} placeholder="exemplo@email.com" />
                 </div>
                 <div className={styles.leabelInput}>
-                  <label>SENHA</label>
+                  <label className={styles.titleInput}>SENHA</label>
                   <div className={styles.separador}></div>
-                  <input type="text" className={styles.input} placeholder="*************"/>
-                </div>
-                <div className={styles.leabelInput}>
-                  <label>CONFIRMAÇÃO DE SENHA</label>
-                  <div className={styles.separador}></div>
-                  <input type="text"className={styles.input} placeholder="*************"/>
+                  <input type="text" className={styles.input} placeholder="*************" />
                 </div>
                 <input type="Submit" value="CONTINUAR" className={styles.submit} />
               </form>
