77356e88be6faa222df7ffeb4e96734eff19eda2
gitpaulosouza <paulo.asouza@bandtec.com.br>
2022-10-18 13:20:13 -0300

    Cadastro.hook corrigido

--- FrontEnd/src/pages/Cadastro/Cadastro.hook.tsx
+++ FrontEnd/src/pages/Cadastro/Cadastro.hook.tsx
@@ -24,38 +24,41 @@ export default function useCadastro() {
                 <>
                     <div className={styles.left}>
                         <div className={styles.text}>
-                        <div className={styles.title}>
-                            Olá, bem vindo! <br />  
-                            possui uma conta?
-                        </div>
-                        <div className={styles.subtitle} onClick={() => routeChange("/login")}>
-                            Realize seu login
-                        </div>
+                            <div className={styles.title}>
+                            OLÁ, BEM VINDO! <br />  
+                            JÁ POSSUI UMA CONTA?
+                            </div>
+                            <div className={styles.subtitle} onClick={() => routeChange("/login")}>
+                            REALIZE SEU LOGIN
+                            </div>
                         </div>
                         <div className={styles.image}>
-                        <img src={signupImage} alt="imagem do banner" />
+                            <img src={signupImage} alt="imagem do banner" />
                         </div>
-                    </div>
-                    <div className={styles.right}>
+                        </div>
+                        <div className={styles.right}>
                         <div className={styles.cardSignup}>
-                        <div className={styles.cardContent}>
-                            <h1>Cadastro</h1>
+                            <div className={styles.cardContent}>
+                            <h1>CADASTRO</h1>
                             <form className={styles.formSignup}>
-                            <div className={styles.leabelInput}>
-                                <label>Email:</label>
+                                <div className={styles.leabelInput}>
+                                <label>EMAIL</label>
+                                <div className={styles.separador}></div>
                                 <input type="text" className={styles.input} placeholder="exemplo@email.com"/>
-                            </div>
-                            <div className={styles.leabelInput}>
-                                <label>Senha:</label>
+                                </div>
+                                <div className={styles.leabelInput}>
+                                <label>SENHA</label>
+                                <div className={styles.separador}></div>
                                 <input type="text" className={styles.input} placeholder="*************"/>
-                            </div>
-                            <div className={styles.leabelInput}>
-                                <label>Confirmação de Senha:</label>
+                                </div>
+                                <div className={styles.leabelInput}>
+                                <label>CONFIRMAÇÃO DE SENHA</label>
+                                <div className={styles.separador}></div>
                                 <input type="text"className={styles.input} placeholder="*************"/>
-                            </div>
-                            <button onClick={() => null} className={styles.submit}>Continuar</button>
+                                </div>
+                                <input type="Submit" value="CONTINUAR" className={styles.submit} />
                             </form>
-                        </div>
+                            </div>
                         </div>
                     </div>
                 </>
