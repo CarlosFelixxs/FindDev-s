0069f25a52e4c01d148f02bf78c0fa0541c1c548
daniloFerreira19 <danilo.ferreira@bandtec.com.br>
2022-11-26 23:11:58 -0300

    criando vaga response dto

--- BackEnd/find-dev-service/src/main/java/projetopi/finddevservice/dtos/v1/response/VagaResponseDto.java
+++ BackEnd/find-dev-service/src/main/java/projetopi/finddevservice/dtos/v1/response/VagaResponseDto.java
@@ -1,4 +1,17 @@
 package projetopi.finddevservice.dtos.v1.response;
 
-public class VagaResponseDto {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.dozermapper.core.Mapping;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+
+public class VagaResponseDto extends RepresentationModel<VagaResponseDto> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Mapping("id")
+    @JsonProperty("id")
+    private int key;
+
 }
